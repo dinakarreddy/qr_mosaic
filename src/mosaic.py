486b5619ebf01a4f8cabba2c0ff8d0230c4c32e8
dinakarreddy <karra.dinakar@gmail.com>
2021-07-17 13:22:41 +0530

    Mosaic docString added

--- src/mosaic.py
+++ src/mosaic.py
@@ -1,38 +1,49 @@
-from PIL import Image
 import numpy as np
 import os
 import random
+from PIL import Image
 
 
 def get_tile_split_masks(split_len, data):
     data_dict = {}
     qr_len = len(data)
-    n = int(qr_len/split_len)
+    n = int(qr_len / split_len)
     for i in range(split_len):
         for j in range(split_len):
-            i_end_index = (i+1)*n
-            j_end_index = (j+1)*n
+            i_end_index = (i + 1) * n
+            j_end_index = (j + 1) * n
             if i == split_len - 1:
                 i_end_index = None
             if j == split_len - 1:
                 j_end_index = None
-            data_dict[(i, j)] = np.copy(data[i*n:i_end_index, j*n:j_end_index])
+            data_dict[(i, j)] = np.copy(data[i * n:i_end_index, j * n:j_end_index])
     return data_dict
 
 
-def main():
-    img_file_path = '/qr/images/1.jpeg'
-    final_img_file_path = '/qr/images/result_images/mosaic.jpeg'
-    tile_directory = '/qr/images/image_set'
-    tile_to_user_img_pixel_ratio = 50
-    tile_size = 500
+def main(img_file_path='images/1.jpeg', final_img_file_path='images/result_images/mosaic.jpeg',
+         tile_directory='images/image_set', tile_size=1000, tile_to_user_img_pixel_ratio=50, user_img_len=2000,
+         user_img_breadth=2000):
+    """
+    :param img_file_path: The image that needs to be made as a mosaic
+    :param final_img_file_path: Final mosaic image will be stored at this path
+    :param tile_directory: The directory from which images are picked up to make part of mosaic image
+    :param tile_size: Size in pixels of each tile in the final mosaic image
+    :param tile_to_user_img_pixel_ratio: This will decide the number of sub images inside the mosaic
+    :param user_img_len: The image that needs to be made as a mosaic will be resized to this
+    :param user_img_breadth: The image that needs to be made as a mosaic will be resized to this
+
+    The function creates a mosaic image at final_img_file_path
+    The final image will contain the following number of sub images:
+        (user_img_len/tile_to_user_img_pixel_ratio) * (user_img_breadth/tile_to_user_img_pixel_ratio)
+    tile_size decides the number of pixels each sub image will contain, so this will decide the clarity of each image.
+        Higher the number, higher the quality with the downside being increased size
+    """
     tiles_data = []
     for filename in os.listdir(tile_directory):
         if filename.endswith(".jpeg"):
             tiles_data.append(np.array(
                 Image.open(os.path.join(tile_directory, filename)).convert('RGB').resize((tile_size, tile_size))))
     user_img = Image.open(img_file_path)
-    user_img_len, user_img_breadth = 500, 500
     user_img = user_img.resize((user_img_len, user_img_breadth))
     user_img_data = np.array(user_img)
     tiles_split_data_dict = [get_tile_split_masks(tile_to_user_img_pixel_ratio, tile_data) for tile_data in tiles_data]
@@ -46,12 +57,12 @@ def main():
         row_list = []
         for c in range(user_img_len):
             index = ((r % tile_to_user_img_pixel_ratio), (c % tile_to_user_img_pixel_ratio))
-            img_index = (int(r/tile_to_user_img_pixel_ratio), int(c/tile_to_user_img_pixel_ratio))
+            img_index = (int(r / tile_to_user_img_pixel_ratio), int(c / tile_to_user_img_pixel_ratio))
             if img_index not in img_index_dict:
                 img_index_dict[img_index] = random.randrange(len(tiles_split_img_dict))
             tile_split_img = tiles_split_img_dict[img_index_dict[img_index]][index]
             tint_img = Image.new('RGB', tile_split_img.size, tuple(user_img_data[r, c]))
-            new_data = np.array(Image.blend(tile_split_img, tint_img, 0.2))
+            new_data = np.array(Image.blend(tile_split_img, tint_img, 0.8))
             print(r, c, user_img_breadth, user_img_len, tile_size, user_img_data[r, c])
             row_list.append(new_data)
         column_list.append(np.concatenate(row_list, axis=1))
